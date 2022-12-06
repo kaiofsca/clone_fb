f23f1122e6b219c8a6511fb9a3367f2ecbad8b6a
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-12-06 15:34:13 -0300

    add login com facebook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import GlobalStyles from './styles/global'
+import { AuthContext } from "./contexts/auth";
+import Login from './components/Login';
 
 const App = () => {
+  const { user }  = useContext(AuthContext);
+
+  if (!user) return <Login />
+
+  console.log(user)
+
   return (
     <>
         bababoi
