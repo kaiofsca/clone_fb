5e40eb238ac0a73c557fa9a3bf65a588df2aa29d
Kaio Fonseca <mkaiofonseca@gmail.com>
2023-01-04 15:09:52 -0300

    voltando a ativa

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import * as C from "./styles"
+import { SiFacebook, SiNintendogamecube, SiMessenger } from "react-icons/si"
+import { FiSearch, FiMenu } from "react-icons/fi"
+import {
+    MdOndemandVideo,
+    MdHome,
+    MdGroups,
+    MdNotifications,
+} from "react-icons/md"
+import { FaStore, FaCaretDown } from "react-icons/fa"
+import { CgMenuGridO } from "react-icons/cg"
+import HeaderIcon from "../HeaderIcon"
+import { AuthContext } from "../../contexts/auth"
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
